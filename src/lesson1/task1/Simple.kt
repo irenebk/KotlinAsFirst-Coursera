7095a487c18ddc1390e1c38c3b623ee039cd57f9
Irina.Bukharova <Irina.Bukharova@nexign-systems.com>
2019-05-09 11:08:30 +0300

    Решены задачи 1 урока

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -48,8 +48,12 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main(args: Array<String>) {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+   // val result = angleInRadian(55,45,9)
+    //println("angelInRadian=$result")
+    val result = thirdDigit(45)
+    println(result)
+    //val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
+    //println("Root product: $x1x2")
 }
 
 /**
@@ -75,7 +79,12 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    val d:Double;
+    d = ((min+sec/60.00)/60.00).toDouble();
+    println(d);
+    return (d+deg)*PI/180.00
+}
 
 /**
  * Тривиальная
@@ -91,7 +100,12 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int
+{
+    println(number)
+    var res = ((number%1000-number%100)/100)
+    return res;
+}
 
 /**
  * Простая
